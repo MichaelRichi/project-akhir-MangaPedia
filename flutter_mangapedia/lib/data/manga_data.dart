7450ff4b45ddc4c50b2c355b94501adfcdcdcb0b
JordiWillyam <jordiwillyamadikusuma_2226240138@mhs.mdp.ac.id>
2024-12-26 04:24:00 +0700

    ADD: 4 manga

--- flutter_mangapedia/lib/data/manga_data.dart
+++ flutter_mangapedia/lib/data/manga_data.dart
@@ -4,10 +4,11 @@ var mangaList = [
   Manga(
     title: 'Boruto: Naruto Next Generations',
     chapter: '81',
-    genre:  'Action, Adventure, Fantasy',
+    genre: 'Action, Adventure, Fantasy',
     releaseDate: 'May 9, 2016',
     malScore: '6.80',
-    author: 'Kishimoto, Masashi (Story), Ikemoto, Mikio (Art), Kodachi, Ukyou (Story)',
+    author:
+        'Kishimoto, Masashi (Story), Ikemoto, Mikio (Art), Kodachi, Ukyou (Story)',
     status: 'Finished',
     serialization: 'Tonari no Young Jump',
     animeAdaptation: 'Boruto: Naruto Next Generations',
@@ -27,5 +28,102 @@ var mangaList = [
     ],
     mangaLink: 'https://mangaplus.shueisha.co.jp/titles/100006',
   ),
- 
+  Manga(
+    title: 'Naruto & Naruto Shippuden',
+    chapter: '700',
+    genre: 'Action, Adventure, Fantasy',
+    releaseDate: 'September 21, 1999',
+    malScore: '8.07',
+    author: 'Masashi Kishimoto (Story & Art)',
+    status: 'Finished',
+    serialization: 'Weekly Shounen Jump',
+    animeAdaptation: 'Naruto',
+    synopsis: '''
+  Setiap kali Naruto Uzumaki menyatakan bahwa suatu hari dia akan menjadi Hokage—gelar yang diberikan kepada ninja terbaik di Desa Konoha—tidak ada yang menganggapnya serius. Sejak lahir, Naruto dijauhi dan dicemooh oleh para penduduk desa. Namun, penghinaan mereka bukan karena Naruto keras kepala, nakal, atau tidak pandai dalam seni ninja, melainkan karena ada iblis di dalam dirinya. Sebelum Naruto lahir, Rubah Ekor Sembilan yang kuat dan mematikan menyerang desa. Untuk menghentikan amukan tersebut, Hokage Keempat mengorbankan nyawanya untuk menyegel iblis itu ke dalam tubuh Naruto yang baru lahir.
+
+  Ketika dia ditugaskan ke Tim 7—bersama rekan-rekan barunya, Sasuke Uchiha dan Sakura Haruno, di bawah bimbingan ninja veteran Kakashi Hatake—Naruto dipaksa untuk bekerja sama dengan orang lain untuk pertama kalinya dalam hidupnya. Melalui pelatihan yang keras dan menjalani misi-misi yang menantang, Naruto harus belajar apa arti bekerja dalam tim dan meniti jalannya sendiri untuk menjadi ninja sejati yang diakui oleh desanya.
+''',
+    imageAsset: 'images/naruto.jpg',
+    imageUrls: [
+      'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1435526149l/1846794.jpg',
+      'https://static1.cbrimages.com/wordpress/wp-content/uploads/2024/11/naruto-manga-volume-45-cover-art.jpg',
+      'https://m.media-amazon.com/images/I/71QxUxK-khL._AC_UF894,1000_QL80_.jpg',
+      'https://ebooks.gramedia.com/ebook-covers/54816/image_highres/BLK_N52020833004.jpg',
+    ],
+    mangaLink: 'https://mangaplus.shueisha.co.jp/titles/100018',
+  ),
+  Manga(
+    title: 'One Piece',
+    chapter: '1134+ (Ongoing)',
+    genre: 'Action, Adventure, Fantasy',
+    releaseDate: 'July 22, 1997',
+    malScore: '9.22',
+    author: 'Eiichiro Oda (Story & Art)',
+    status: 'Ongoing',
+    serialization: 'Weekly Shounen Jump',
+    animeAdaptation: 'One Piece',
+    synopsis: '''
+  Gol D. Roger, seorang pria yang dikenal sebagai Raja Bajak Laut, akan dieksekusi oleh Pemerintah Dunia. Namun, tepat sebelum kematiannya, ia mengonfirmasi keberadaan harta karun besar bernama One Piece yang tersembunyi di suatu tempat di lautan luas yang dikenal sebagai Grand Line. Mengumumkan bahwa One Piece dapat dimiliki oleh siapa saja yang cukup layak untuk menemukannya, Raja Bajak Laut dieksekusi, dan Zaman Keemasan Bajak Laut pun dimulai.
+
+Dua puluh dua tahun kemudian, seorang pemuda bernama Monkey D. Luffy siap memulai petualangannya sendiri untuk mencari One Piece dan berjuang menjadi Raja Bajak Laut yang baru. Dengan hanya berbekal topi jerami, perahu kecil, dan tubuh elastis, ia memulai perjalanan luar biasa untuk mengumpulkan kru sendiri dan kapal yang layak untuk membawa mereka melintasi Grand Line demi meraih gelar tertinggi di lautan.
+''',
+    imageAsset: 'images/onepiece.jpg',
+    imageUrls: [
+      'https://awsimages.detik.net.id/community/media/visual/2024/04/20/top-7-cover-terbaik-manga-one-piece-6_34.webp?w=375',
+      'https://awsimages.detik.net.id/community/media/visual/2024/02/20/manga-one-piece-volume-108.jpeg?w=1200',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTV3Rma_V_mfduiLXtAXT1QBEuGQiSWG_60Mg&s',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwbTbYvfqtR01PM9daJh-MGS7HJ5s-9sSPfA&s',
+    ],
+    mangaLink: 'https://mangaplus.shueisha.co.jp/titles/100140',
+  ),
+  Manga(
+    title: 'Dragon Ball',
+    chapter: '520',
+    genre: 'Action, Adventure, Fantasy',
+    releaseDate: 'Nov 20, 1984',
+    malScore: '8.43',
+    author: 'Akira Toriyama (Story & Art)',
+    status: 'Finished',
+    serialization: 'Weekly Shounen Jump',
+    animeAdaptation: 'Dragon Ball, Dragon Ball Z',
+    synopsis: '''
+  Bulma, seorang gadis berusia 16 tahun yang keras kepala, sedang dalam misi untuk menemukan Bola Naga yang legendaris—tujuh bola ajaib yang tersebar dan dapat mengabulkan satu permintaan bagi siapa pun yang menemukannya. Bulma hanya memiliki satu keinginan: mendapatkan pacar sempurna. Dalam perjalanannya, Bulma bertemu dengan Son Goku, seorang anak yatim piatu yang kuat dan hanya pernah mengenal satu manusia selain dirinya. Gokuu memiliki salah satu Bola Naga sebagai kenang-kenangan dari kakeknya yang telah tiada. Sebagai gantinya, Bulma mengajak Goku untuk menjadi teman dalam petualangannya.
+
+  Bersama Bulma, Goku menemukan dunia yang benar-benar asing baginya. Musuh-musuh kuat juga berlomba-lomba mengejar Bola Naga, memaksa Goku untuk melampaui batasnya demi melindungi Bulma dan lingkaran sekutu mereka yang terus bertambah. Namun, Goku menyimpan rahasia yang bahkan tidak ia sadari; kekuatan luar biasa dalam dirinya berasal dari sumber misterius yang dapat mengancam banyak orang yang ia sayangi.
+
+  Saat keahliannya dalam seni bela diri berkembang, Goku menarik lawan yang lebih kuat dengan rencana jahat yang dapat runtuh di bawah kekuatannya. Ia memulai perjalanan tanpa akhir untuk berlatih dan bertarung demi melindungi orang-orang tercinta dan nasib planet ini.
+  ''',
+    imageAsset: 'images/dragonball.jpg',
+    imageUrls: [
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmLSkFvwqKpT74EUFUNhXP-7QU90EwJUu8Hw&s',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvH9c1_unMb7Csb6C71vfRX7b-LWWJDD3MIw&s',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_xnC5MPmkK_FdAYlXYKpQFeg_24R1gTborA&s',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzRSV8rZYnjexzgiInlkHbD618hebsc25_dg&s',
+    ],
+    mangaLink: 'https://mangaplus.shueisha.co.jp/titles/100011',
+  ),
+  Manga(
+    title: 'Bleach',
+    chapter: '705',
+    genre: 'Action, Adventure, Supernatural',
+    releaseDate: 'August 7, 2001',
+    malScore: '7.88',
+    author: 'Tite Kubo (Story & Art)',
+    status: 'Finished',
+    serialization: 'Weekly Shounen Jump',
+    animeAdaptation: 'Bleach',
+    synopsis: '''
+  Sejak kecil, siswa SMA Ichigo Kurosaki selalu bisa melihat roh orang yang telah meninggal. Namun, kemampuan tersebut tidak menghalanginya untuk menjalani kehidupan yang normal. Suatu hari, Ichigo kembali ke rumah dan menemukan seorang penyusup di kamarnya yang memperkenalkan dirinya sebagai Rukia Kuchiki, seorang Shinigami yang bertugas membantu jiwa-jiwa untuk berpindah ke alam baka. Percakapan mereka tiba-tiba terhenti ketika muncul Hollow—roh jahat yang dikenal karena memangsa jiwa. Saat Ichigo nekat mencoba menghentikan Hollow tersebut, Rukia turun tangan untuk melindunginya dari serangan balasan. Terluka parah dan tidak mampu melanjutkan pertarungan, Rukia mengusulkan rencana berisiko—memindahkan setengah kekuatan Shinigami-nya ke Ichigo. Ichigo menyetujui, dan secara mengejutkan, ia menyerap seluruh kekuatan Rukia, memungkinkannya untuk dengan mudah mengalahkan Hollow itu.
+
+Kini menjadi seorang Shinigami, Ichigo harus mengambil alih tugas Rukia untuk membasmi Hollow dan melindungi roh, baik yang masih hidup maupun yang sudah mati. Bersama teman-temannya, Orihime Inoue dan Yasutora Sado—yang kemudian juga menemukan kemampuan spiritual mereka masing-masing—Ichigo segera menyadari bahwa konsekuensi dari menjadi seorang Shinigami dan berurusan dengan dunia roh jauh lebih besar daripada yang pernah ia bayangkan.
+''',
+    imageAsset: 'images/bleach.jpg',
+    imageUrls: [
+      'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781421542980/bleach-vol-45-9781421542980_hr.jpg',
+      'https://tradejapanstore.com/cdn/shop/products/9784088738833.jpg?v=1683249649',
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgV1YhIk6ceVJQsHAWJN7Te0cu7wtSHy12bw&s',
+      'https://i.redd.it/mfsssge0xhpa1.png',
+    ],
+    mangaLink: 'https://mangaplus.shueisha.co.jp/titles/100004',
+  ),
 ];
